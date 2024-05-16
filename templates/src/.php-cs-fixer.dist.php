9500a7e80dacf2c19d436fe4ab90e55da981dd58
Elias Häußler <e.haeussler@familie-redlich.de>
2024-05-16 07:56:00 +0200

    [FEATURE] Use parallel execution for PHP-CS-Fixer

--- templates/src/.php-cs-fixer.dist.php
+++ templates/src/.php-cs-fixer.dist.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 
 $config = \TYPO3\CodingStandards\CsFixerConfig::create();
+$config->setParallelConfig(\PhpCsFixer\Runner\Parallel\ParallelConfigFactory::detect());
 $config->getFinder()->in(__DIR__ . '/packages');
 
 return $config;
