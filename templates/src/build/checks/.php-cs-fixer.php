c11a9b153a902b82ef2ae882f37cedaf6bda5ab4
Elias Häußler <elias@haeussler.dev>
2026-07-26 18:59:12 +0200

    [TASK] Restructure build files and use shared workflows

--- templates/src/build/checks/.php-cs-fixer.php
+++ templates/src/build/checks/.php-cs-fixer.php
@@ -4,6 +4,6 @@ declare(strict_types=1);
 
 $config = \TYPO3\CodingStandards\CsFixerConfig::create();
 $config->setParallelConfig(\PhpCsFixer\Runner\Parallel\ParallelConfigFactory::detect());
-$config->getFinder()->in(__DIR__ . '/packages');
+$config->getFinder()->in(dirname(__DIR__, 2) . '/packages');
 
 return $config;
